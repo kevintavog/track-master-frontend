f07944069869497ef96c58c9c5ec1e31bf90a494
kevintavog <kevin.tavog@zoho.com>
2019-07-14 10:21:18 -0700

    Add paging to the bottom of the search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -95,6 +95,20 @@
       </template>
 
     </b-table>
+    <br>
+    <b-pagination
+      :total="searchResults.totalMatches"
+      :current.sync="currentPage"
+      :per-page="pageSize"
+      :rounded="true"
+      icon-pack="fas"
+      order="is-centered"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page">
+    </b-pagination>
+
   </div>
 </template>
 
